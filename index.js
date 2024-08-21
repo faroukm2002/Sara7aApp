e61dadcf02cd79c96b3ae82368114e4b1ad163da
faroukm2002 <faroukm238@gamil.com>
2024-08-21 20:17:01 +0300

    create signUp and signIn(routes)

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import { dbConnection } from './database/dbConnection.js'
+import userRouter from './src/modules/user/user.routes.js'
+import dotenv from 'dotenv';
+
 const app = express()
 const port = 3000
 app.use(express.json())
+dotenv.config(); 
 
+
+app.use(userRouter)
 dbConnection()
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
