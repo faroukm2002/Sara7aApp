ac7cfa28f919ef2906dc54fd89315d8f7cf14491
faroukm2002 <faroukm238@gamil.com>
2024-08-22 21:55:27 +0300

    handle error outside express

--- index.js
+++ index.js
@@ -29,3 +29,8 @@ dbConnection()
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+
+// handle error outside express 
+process.on('unhandledRejection',(err)=>{
+    console.log('errorrrrrrrrrrrrrr', err)
+})
