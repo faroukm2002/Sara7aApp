9df3845d725771efd84389f3d2a5a8c04ed1bbbd
faroukm2002 <faroukm238@gamil.com>
2024-08-23 22:13:39 +0300

    update

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,11 +1,18 @@
-
  import jwt from 'jsonwebtoken';
-export const auth =(req,res,next) => {
-    let token=req.header('token')
-     jwt.verify(token,process.env.JWT_KEY ,async(err,decoded)=>{
-        if(err) return res.json({message:"error in token or not provided token"})
-            req.userId=decoded.userId
-        next()
-    })
+ import { AppError } from '../utils/AppError.js';
+ export const auth =(req,res,next) => {
+     let token=req.header('token')
+     if (!token || !token.startsWith('Bearer ')) {
+        return next(new AppError("Token is required or invalid bearer key", 401));
+    }
 
-}
+   // Remove 'Bearer ' prefix from the token
+    const actualToken = token.split(' ')[1]; 
+
+      jwt.verify(actualToken,process.env.JWT_KEY ,async(err,decoded)=>{
+         if(err) return res.json({message:"error in token or not provided token"})
+             req.userId=decoded.userId
+         next()
+     })
+ 
+ }
